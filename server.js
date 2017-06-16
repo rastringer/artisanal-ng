8aaab474061732b363026b0cc686eb78ebc03205
rastringer <ra1stringer@gmail.com>
2017-06-15 21:18:05 -0700

    switching server port

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var app     = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname + '/app'));
 app.use(express.static(__dirname + '/bower_components'));
 
@@ -12,3 +14,7 @@ app.get('/', function(req, res) {
 
 app.listen(8080);
 console.log('Magic happens on 8080');
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
